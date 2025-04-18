8aab26293fedc5401c73a472b7548f0fa742c394
Suryansh Sinha <suryanshsinha01@gmail.com>
2025-04-18 14:01:07 +0530

    Add dockerfile config

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def greet():
     return f'Hello {user_input}, Welcome to this app for Docker demonstration. Hope it\'s easy to understand!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0', port=3000)
